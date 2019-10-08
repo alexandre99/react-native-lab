43e620273b3e2cb7d0b6b888c699e4087871515f
Alexandre Teixeira <alexanddy99@gmail.com>
2019-10-07 22:07:24 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  Image
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Alexandre Teixeira</Text>
+      <Image></Image>
+    </View>
+  );
+};
+
+export default App;
