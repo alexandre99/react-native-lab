4591ecf9eb796062c547d6b52d0c2d61a46c4cbe
Alexandre Teixeira <alexanddy99@gmail.com>
2019-10-08 08:26:49 -0300

    implementado flatlist

--- App.js
+++ App.js
@@ -10,16 +10,31 @@ import React from 'react';
 import {
   View,
   Text,
-  Image
+  Image,
+  Dimensions,
+  FlatList
 } from 'react-native';
 
+const width = Dimensions.get('screen').width;
+
+const fotos = [
+  { id: 1, usuario: 'Alexandre Teixeira' },
+  { id: 2, usuario: 'Teste 1' },
+  { id: 3, usuario: 'Teste 2' }
+];
 
 const App: () => React$Node = () => {
   return (
-    <View>
-      <Text>Alexandre Teixeira</Text>
-      <Image></Image>
-    </View>
+    <FlatList style={{ marginTop: 20 }}
+      data={fotos}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) =>
+        <View>
+          <Text>{item.usuario}</Text>
+          <Image source={require('./resources/img/rhcp.jpeg')} style={{ width: width, height: width }} />
+        </View>
+      }
+    />
   );
 };
 
