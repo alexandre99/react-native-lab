0adca9c3644910c49648391a26497a27f8b36913
Alexandre Teixeira <alexanddy99@gmail.com>
2019-10-10 08:10:42 -0300

    Implementado função de like

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,20 +5,46 @@ import {
   View,
   Image,
   Dimensions,
-  FlatList
+  TouchableOpacity
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { foto: this.props.foto }
+  }
+
+  carregaIcone(likeada) {
+    return likeada ? require('../../resources/img/s2-check.png') : require('../../resources/img/s2.png')
+  }
+
+  like() {
+    const fotoAtualizada = {
+      ...this.state.foto,
+      likeada: !this.state.foto.likeada
+    }
+
+    this.setState({ foto: fotoAtualizada });
+  }
+
   render() {
+    const { foto } = this.state;
+
     return (
       <View>
         <View style={styles.cabecalho}>
-          <Image source={{ uri: this.props.foto.urlPerfil }} style={styles.fotoDePerfil} />
-          <Text>{this.props.foto.loginUsuario}</Text>
+          <Image source={{ uri: foto.urlPerfil }} style={styles.fotoDePerfil} />
+          <Text>{foto.loginUsuario}</Text>
         </View>
-        <Image source={{ uri: this.props.foto.urlFoto }} style={styles.foto} />
+        <Image source={{ uri: foto.urlFoto }} style={styles.foto} />
+        <View style={styles.rodape}>
+          <TouchableOpacity onPress={this.like.bind(this)}>
+            <Image style={styles.botaoDeLike} source={this.carregaIcone(foto.likeada)} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -38,5 +64,12 @@ const styles = StyleSheet.create({
   foto: {
     width: width,
     height: width
+  },
+  botaoDeLike: {
+    width: 40,
+    height: 40
+  },
+  rodape: {
+    margin: 10
   }
 })
