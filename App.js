55ffa96c5319058e33fd954e8a7a7225a9fcf74f
Alexandre Teixeira <alexanddy99@gmail.com>
2019-10-13 17:38:34 -0300

    refatoração

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import {
-  FlatList,
-  StyleSheet,
-  Platform
-} from 'react-native';
+import { FlatList, Platform, StyleSheet } from 'react-native';
+import Config from 'react-native-config';
 import Post from './src/components/Post';
-import Config from 'react-native-config'
 
 class App extends Component {
 
@@ -21,9 +17,62 @@ class App extends Component {
     fetch(Config.URL)
       .then(resposta => resposta.json())
       .then(json => this.setState({ fotos: json }))
-      .catch(error => { 
-        console.log(error) 
+      .catch(error => {
+        console.log(error)
       });
+  }
+
+  like(idFoto) {
+    const foto = this.state.fotos.find(foto => foto.id === idFoto)
+
+    let novaLista = []
+    if (!foto.likeada) {
+      novaLista = [
+        ...foto.likers,
+        { login: 'meuUsuario' }
+      ]
+    } else {
+      novaLista = foto.likers.filter(liker => {
+        return liker.login !== 'meuUsuario'
+      })
+    }
+
+    const fotoAtualizada = {
+      ...foto,
+      likeada: !foto.likeada,
+      likers: novaLista
+    }
+
+    const fotos = this.state.fotos.
+      map(foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto)
+
+    this.setState({ fotos });
+  }
+
+  adicionaComentario(idFoto, valorComentario, inputComentario) {
+    if (this.state.valorComentario === '')
+      return;
+
+    const foto = this.state.fotos.find(foto => foto.id === idFoto)
+
+    const novaLista = [...foto.comentarios, {
+      id: valorComentario,
+      login: 'meuUsuario',
+      texto: valorComentario
+    }];
+
+    const fotoAtualizada = {
+      ...foto,
+      comentarios: novaLista
+    }
+
+    const fotos = this.state.fotos.
+      map(foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto)
+
+    this.setState({ fotos });
+
+    inputComentario.clear();
+
   }
 
   render() {
@@ -32,9 +81,11 @@ class App extends Component {
         keyExtractor={item => item.id.toString()}
         data={this.state.fotos}
         renderItem={({ item }) =>
-          <Post foto={item} />
+          <Post foto={item}
+            likeCallback={this.like.bind(this)}
+            comentarioCallback={this.adicionaComentario.bind(this)} />
         }
-      ></FlatList>
+      ></FlatList >
     );
   }
 }
